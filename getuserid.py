386c63be4fc8476b6e3243be9caba64672aed1ee
Alexander Bridges <a.yakovlev911@gmail.com>
2017-05-26 22:37:54 +0300

    instagram_get_userid

---
+++ getuserid.py
@@ -0,0 +1,15 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+username = input('username: ')
+try:
+	html = urlopen('https://smashballoon.com/instagram-feed/find-instagram-user-id/?username='+username+'&address=&896914zje22267qjtl=4')
+except:
+	print('Could not get userID')
+	exit()
+if html:
+	bs0bj = BeautifulSoup(html,'lxml')
+	namelist = bs0bj.findAll('div','user match')
+	string = (namelist[0].get_text())
+	a, userid = string.split(': ')
+	print (userid)
