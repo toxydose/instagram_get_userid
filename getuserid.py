904490928b1a794c6d83e0cecc3a97b3ccf6cbc2
mrbridges <mrbridges@qqq>
2017-05-26 22:54:30 +0300

    exceptions added

--- getuserid.py
+++ getuserid.py
@@ -5,11 +5,14 @@ username = input('username: ')
 try:
 	html = urlopen('https://smashballoon.com/instagram-feed/find-instagram-user-id/?username='+username+'&address=&896914zje22267qjtl=4')
 except:
-	print('Could not get userID')
+	print('Could not get userID!')
 	exit()
 if html:
 	bs0bj = BeautifulSoup(html,'lxml')
 	namelist = bs0bj.findAll('div','user match')
-	string = (namelist[0].get_text())
-	a, userid = string.split(': ')
-	print (userid)
+	try:
+		string = (namelist[0].get_text())
+		a, userid = string.split(': ')
+		print (userid)
+	except IndexError:
+		print('Error! Non existent user.')
