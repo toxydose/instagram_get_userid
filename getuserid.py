daffe083055403cc79b4eba431a8013982ef3dd6
mrbridges <a.yakovlev911@gmail.com>
2017-05-31 12:17:31 +0300

    antibot defence passing

--- getuserid.py
+++ getuserid.py
@@ -1,11 +1,27 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
+#-------------------------------------------------------------------
+#---------------retrieve site id (antibot defence)-------------------
+try:
+	html = urlopen('https://smashballoon.com/instagram-feed/find-instagram-user-id/')
+except:
+	print('Could not get userID! Site is down')
+	exit()
+if html:
+	bs0bj = BeautifulSoup(html,'lxml')
+	namelist = bs0bj.findAll({'input','size'})
+	raw = str(namelist[2])
+	b = raw.split('"')
+	secretid = b[1]
+
+#--------------------------------------------------------------------
+#-----------------retrieve userid from username----------------------
 username = input('username: ')
 try:
-	html = urlopen('https://smashballoon.com/instagram-feed/find-instagram-user-id/?username='+username+'&address=&5611zzl19832tjrl=4')
+	html = urlopen('https://smashballoon.com/instagram-feed/find-instagram-user-id/?username='+username+'&address=&'+secretid+'=4')
 except:
-	print('Could not get userID!')
+	print('Could not get userID! Site is down')
 	exit()
 if html:
 	bs0bj = BeautifulSoup(html,'lxml')
